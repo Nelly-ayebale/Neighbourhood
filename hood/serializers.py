30b39b094f9b52af38a9fbb5a437c2eda10b7540
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-12-03 06:45:15 +0300

    Created a Neighbourhood Serializer

--- hood/serializers.py
+++ hood/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from . import models
+from .models import Neighbourhood
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.CustomUser
         fields = ('email', 'username', )
+
+class NeighbourhoodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Neighbourhood
+        fields = '__all__'
