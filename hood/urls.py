c0153bcba14160c0cbc37fddfa78c514d3cc3f66
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-12-03 07:22:58 +0300

    Added a permissions.py file for permissions for users

--- hood/urls.py
+++ hood/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from views import index,HoodSetView
+from .views import index,HoodSetView
 from rest_framework.routers import DefaultRouter
 from django.conf import settings
 from django.conf.urls.static import static
