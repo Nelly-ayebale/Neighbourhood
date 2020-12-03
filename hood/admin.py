1bebbcfac45833a15e9a63f85cda1aee93109c11
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-12-03 18:16:04 +0300

    Deployment to Heroku

--- hood/admin.py
+++ hood/admin.py
@@ -11,7 +11,7 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username', 'name','profile_photo','status']
+    list_display = ['email', 'username']
 
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Neighbourhood)
