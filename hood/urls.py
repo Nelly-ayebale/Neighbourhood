36b853cfabadd7023d040c92324aa0a4e9ef29b6
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-12-03 06:57:54 +0300

    Created urls for the endpoint of hood

--- hood/urls.py
+++ hood/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 from . import views
+from views import index,HoodSetView
+from rest_framework.routers import DefaultRouter
 from django.conf import settings
 from django.conf.urls.static import static
 
+router = DefaultRouter()
+router.register(r"hood",HoodSetView)
 
 urlpatterns=[
     path('index/',views.index,name='index'),
     path('', views.UserListView.as_view()),
-]
+] + router.urls
 
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- hood/views.py
+++ hood/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from rest_framework import generics
 from . import models
 from . import serializers
-
+from .serializers import NeighbourhoodSerializer
+from rest_framework import viewsets
+from .models import Neighbourhood
 # Create your views here.
 def index(request):
     return render(request,'hoods/home.html')
@@ -11,3 +13,7 @@ class UserListView(generics.ListAPIView):
     queryset = models.CustomUser.objects.all()
     serializer_class = serializers.UserSerializer
 
+class HoodSetView(viewsets.ModelViewSet):
+    serializer_class = NeighbourhoodSerializer
+    queryset = Neighbourhood.objects.all()
+    
